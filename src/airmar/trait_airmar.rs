3bce65ee881f979e4fbec975c61fab7baaa7407e
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-02-04 14:54:18 -0800

    added static checksum and bytes to Vec<u8> helper method

--- src/airmar/trait_airmar.rs
+++ src/airmar/trait_airmar.rs
@@ -1,9 +1,20 @@
 
 use std::pin::Pin;
 use std::future::Future;
-use super::models::AirmarTx;
+use super::models::{AirmarTx, SOP, END_PACKET_BYTES};
 
 pub(crate) trait AirmarT {
     fn run(&self, tx:AirmarTx) ->
         Pin<Box<dyn Future<Output= anyhow::Result<()>> + Send>>;
+
+    fn package_sentence(s: &str) -> Vec<u8> {
+        let checksum = s.bytes().fold(0u8, |acc, b| acc ^ b);
+        let complete = format!("{}{}*{:02X}{}", 
+            SOP, 
+            s, 
+            checksum,
+            std::str::from_utf8(&END_PACKET_BYTES).unwrap()
+        );
+        complete.into_bytes()
+    }
 }
