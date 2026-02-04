8b95dac33f44ad75135abea3b400403e2f0c4001
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-02-04 14:44:35 -0800

    generalized the function for trait to not just be for weather

--- src/airmar/trait_airmar.rs
+++ src/airmar/trait_airmar.rs
@@ -4,6 +4,6 @@ use std::future::Future;
 use super::models::AirmarTx;
 
 pub(crate) trait AirmarT {
-    fn detect_weather_task(&self, tx: AirmarTx) -> 
+    fn run(&self, tx:AirmarTx) ->
         Pin<Box<dyn Future<Output= anyhow::Result<()>> + Send>>;
 }
