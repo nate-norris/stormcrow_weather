ea47642759a30b3480108fe79ca9965fd959d072
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-02-04 14:44:05 -0800

    made mock AirmarT public in module

--- src/airmar.rs
+++ src/airmar.rs
@@ -1,9 +1,11 @@
 // 
 pub mod models;
 pub mod sensor;
+pub mod sensor_mock;
 pub(crate) mod nmea_sentence;
 pub(crate) mod trait_airmar;
 
 
 pub use models::AirmarTx;
 pub use sensor::AirmarSensorReal;
+pub use sensor_mock::AirmarSensorMock;
