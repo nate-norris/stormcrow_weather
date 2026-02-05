a854a82209147297aaf2af56edb9c376f9431969
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-02-05 05:50:24 -0800

    working through Trait change

--- src/airmar/sensor.rs
+++ src/airmar/sensor.rs
@@ -1,33 +1,72 @@
+use tokio::serial::SerialStream;
 use super::models::AirmarTx;
 use super::trait_airmair::AirmarT;
 use super::nmea_sentence::NMEASentenceRetriever;
 
 pub struct AirmarSensorReal;
 
-impl AirmarT for AirmarSensorReal {
-    fn detect_weather_task(&self, tx: AirmarTx) -> 
-        Pin<Box<dyn Future<Output= anyhow::Result<()>> + Send>> {
+/*
+open port
+send disable-all command except WIMDA
+send GGA query (one time read)
+wait for GGA
 
+stream WIMDA forever
+ */
+
+impl AirmarT for AirmarSensorReal {
+
+    fn run(&self, tx:AirmarTx) ->
+        Pin<Box<dyn Future<Output= anyhow::Result<()>> + Send>> {
         Box::pin(async move {
             let mut port = tokio_serial::new("/dev/ttyUSB1", 4_800)
                 .open_native_async()?;
-            let mut buf = [0u8; 64];
-            let sentence = NMEASentenceRetriever::new();
 
-            loop {
-                let n = port.read(&mut buf).await?;
+            // turn off all not needed sentences
+            //      Keep WIMDA sentences active.
+            //      GPGGA will be read as a query only 
+            configure_sentence_transmissions(&mut port).await?;
 
-                if n == 0 {
-                    continue;
-                }
-
-                // process only new bytes read
-                for &byte in &buf[..n] {
-                    if let Some(sentence_str) = sentence.push(byte)? {
-                        tx.send(sentence_str).await?;
-                    }
-                }
-            }
+            // query for the altitude
+            detect_altitude(&mut port, tx.clone()).await?;
+            
+            // listen for weather
+            detect_weather(&mut port, tx).await?;
+            Ok(())
         })
     }
-}             
+}
+
+async fn configure_sentence_transmissions(port: &mut SerialStream) 
+    -> anyhow::Result<()> {
+    Ok(())
+}
+
+async fn detect_altitude(port: &mut SerialStream, tx: AirMarTx) ->
+    anyhow::Result<()> {
+    //$GPQ,GGA*CS\r\n
+    Ok(())
+}
+
+async fn detect_weather(port: &mut SerialStream, tx: AirmarTx) -> 
+    anyhow::Result<()> {
+
+    //TODO enable weather transmissions
+    let mut buf = [0u8; 64];
+    let sentence = NMEASentenceRetriever::new();
+
+    loop {
+        let n = port.read(&mut buf).await?;
+
+        if n == 0 {
+            continue;
+        }
+
+        // process only new bytes read
+        for &byte in &buf[..n] {
+            if let Some(sentence_str) = sentence.push(byte)? {
+                tx.send(sentence_str).await?;
+            }
+        }
+    }
+}
