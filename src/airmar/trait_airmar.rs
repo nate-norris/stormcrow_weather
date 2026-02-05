9a4d64fe34762c7c257d58813e6012e428069279
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-02-05 12:59:35 -0800

    fixed spacing and indentation

--- src/airmar/trait_airmar.rs
+++ src/airmar/trait_airmar.rs
@@ -9,7 +9,8 @@ pub(crate) trait AirmarT {
         Pin<Box<dyn Future<Output= anyhow::Result<()>> + Send>>;
 
     fn package_sentence(s: &str) -> Vec<u8> {
-        let checksum = s.bytes().fold(0u8, |acc, b| acc ^ b);
+        let checksum = s.bytes().fold(0u8, |acc, 
+            b| acc ^ b);
         let complete = format!("{}{}*{:02X}{}", 
             SOP, 
             s, 
@@ -19,7 +20,11 @@ pub(crate) trait AirmarT {
         complete.into_bytes()
     }
 
-    async fn transmit_bytes(bytes: &[u8], sentence_retriever: &mut NMEASentenceRetriever, airmar_tx: AirmarTx) -> anyhow::Result<()> {
+    async fn transmit_bytes(bytes: &[u8], 
+        sentence_retriever: &mut NMEASentenceRetriever, 
+        airmar_tx: &AirmarTx) -> anyhow::Result<()> {
+            
+        //iterate bytes and transmit sentence if complete
         for &byte in bytes {
             if let Some(complete_sentence) = sentence_retriever.push(byte)? {
                 airmar_tx.send(complete_sentence).await?;
