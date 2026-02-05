f9e7ab8800e5cf4b5f76dd00a2ee26b6b1425662
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-02-05 12:58:57 -0800

    updated spacing

--- src/airmar.rs
+++ src/airmar.rs
@@ -5,7 +5,6 @@ pub mod sensor_mock;
 pub(crate) mod nmea_sentence;
 pub(crate) mod trait_airmar;
 
-
 pub use models::AirmarTx;
 pub use sensor::AirmarSensorReal;
 pub use sensor_mock::AirmarSensorMock;
