5dfc5d919df42699ffa676f9a162f4ef32767074
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-02-12 12:05:52 -0800

    updated imports

--- src/airmar.rs
+++ src/airmar.rs
@@ -4,9 +4,9 @@ pub mod sensor;
 pub mod sensor_mock;
 pub mod trait_airmar;
 pub(crate) mod nmea_sentence;
+pub(crate) mod interpreter;
 
-
-pub use models::AirmarTx;
+pub use models::AirmarEventTx;
 pub use sensor::AirmarSensorReal;
 pub use sensor_mock::AirmarSensorMock;
 pub use trait_airmar::AirmarT;
