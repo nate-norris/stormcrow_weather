0c7b3e06ce613c37ce0ad1785f49f389bb2e1420
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-02-12 12:07:34 -0800

    updated tx sender and added ExpectedSentence

--- src/airmar/models.rs
+++ src/airmar/models.rs
@@ -9,5 +9,31 @@ pub(crate) enum NMEASentenceState {
     ReadSentence,
 }
 
-// raw transmission of nmea sentences
-pub type AirmarTx = mpsc::Sender<String>;
+pub enum AirmarEvent {
+    Post(bool),
+    Altitude { meters: f32 },
+    Wimda {
+        wind_full: f32,
+        wind_dir: f32,
+        temp: f32,
+        humidity: f32,
+        baro: f32,
+    },
+}
+// event ready for consumer
+pub type AirmarEventTx = mpsc::Sender<AirmarEvent>;
+
+pub(crate) enum ExpectedSentence {
+    Post,
+    Alt,
+    Wimda
+}
+impl ExpectedSentence {
+    pub(crate) fn prefix(&self) -> &'static str {
+        match self {
+            ExpectedSentence::Post => "$PAMTR,POST",
+            ExpectedSentence::Alt => "$PAMTR,ALT",
+            ExpectedSentence::Wimda => "$WIMDA",
+        }
+    }
+}
