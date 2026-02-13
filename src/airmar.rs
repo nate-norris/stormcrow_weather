22a92bdd26bfb4ca48aa421139610f334a1f5ccf
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-02-13 09:12:15 -0800

    exposed AirmarEvent

--- src/airmar.rs
+++ src/airmar.rs
@@ -6,7 +6,7 @@ pub mod trait_airmar;
 pub(crate) mod nmea_sentence;
 pub(crate) mod interpreter;
 
-pub use models::AirmarEventTx;
+pub use models::{AirmarEventTx, AirmarEvent};
 pub use sensor::AirmarSensorReal;
 pub use sensor_mock::AirmarSensorMock;
 pub use trait_airmar::AirmarT;
