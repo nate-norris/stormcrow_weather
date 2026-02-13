c5c8ab2379d978f81aeb4a62b647c0c1e6553284
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-02-13 11:35:56 -0800

    added ConsumerState enum

--- src/airmar_consumer/models.rs
+++ src/airmar_consumer/models.rs
@@ -3,3 +3,9 @@ use tokio::sync::mpsc;
 use crate::airmar::AirmarEvent;
 
 pub type AirmarEventRx = mpsc::Receiver<AirmarEvent>;
+
+pub(crate) enum ConsumerState {
+    WaitingForPOST,
+    WaitingForAltitude,
+    ReadyForWeather,
+}
