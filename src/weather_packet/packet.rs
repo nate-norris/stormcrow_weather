00900c05dfe37ee23946abc08f36f85357093e7f
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-03-31 10:00:33 -0700

    updated payload by adding struct definition

--- src/weather_packet/packet.rs
+++ src/weather_packet/packet.rs
@@ -1,5 +1,37 @@
+//! Contains the WeatherPacket struct implementing trait PacketT
+//! for uniform byte structuring of MM2T serial packets.
+//! 
+//! WeatherPayload defines the layout of paylod in WeatherPacket.
 use utils::mm2t::PacketT;
 use super::site_id::get_site_char;
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+struct WeatherPayload {
+    site: u8,
+    altitude: f32,
+    wind_full: f32,
+    wind_dir: f32,
+    temp: f32,
+    humidity: f32,
+    baro: f32,
+}
+
+impl WeatherPayload {
+    fn encode(self) -> Vec<u8> {
+        let mut buf = Vec::with_capacity(25);
+
+        buf.push(self.site);
+        buf.extend(self.altitude.to_le_bytes());
+        buf.extend(self.wind_full.to_le_bytes());
+        buf.extend(self.wind_dir.to_le_bytes());
+        buf.extend(self.temp.to_le_bytes());
+        buf.extend(self.humidity.to_le_bytes());
+        buf.extend(self.baro.to_le_bytes());
+
+        buf
+    }
+}
 
 pub struct WeatherPacket {
     payload: Vec<u8>,
@@ -7,18 +39,21 @@ pub struct WeatherPacket {
 
 impl WeatherPacket {
     pub fn new(altitude: f32, wind_full: f32, wind_dir: f32, temp: f32, 
-        humdity: f32, baro: f32) -> Self {
+        humidity: f32, baro: f32) -> Self {
 
-        let mut buf = Vec::with_capacity(25);
-        buf.push(get_site_char() as u8);
-        buf.extend_from_slice(&altitude.to_le_bytes());
-        buf.extend_from_slice(&wind_full.to_le_bytes());
-        buf.extend_from_slice(&wind_dir.to_le_bytes());
-        buf.extend_from_slice(&temp.to_le_bytes());
-        buf.extend_from_slice(&humdity.to_le_bytes());
-        buf.extend_from_slice(&baro.to_le_bytes());
+        assert!(get_site_char().is_ascii());
+        let payload = WeatherPayload {
+            site: get_site_char() as u8,
+            altitude,
+            wind_full,
+            wind_dir,
+            temp,
+            humidity,
+            baro,
+        }
+        .encode();
         
-        Self { payload: buf }
+        Self { payload }
     }
 }
 
