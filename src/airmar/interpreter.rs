9f6d6dbd6a1e60a58b9699e5a3c53e09ce648f09
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-07-07 08:38:28 -0700

    documented POST structure, testing logger

--- src/airmar/interpreter.rs
+++ src/airmar/interpreter.rs
@@ -1,27 +1,54 @@
 use nmea::{parse_nmea_sentence, sentences::mda::parse_mda};
 use super::models::AirmarEvent;
+use utils::logger;
 
 /// Confirm Power On Self Test (POST) response is clear.
 /// If clear all values will be "0" for present sensors.
 /// 
-/// The airmar will provide sentences in the following structure 
+/// The airmar will provide sentences in the following structure:
 ///     $PAMTR,POST,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,,,,,WX
 ///     $PAMTR,POST,0,0,0,0,0,0,0,0,0,0,0,0,0,,0,0,,,,,WX
+///     WXS1500 has provided: $PAMTR,POST,0,0,0,0,0,,0,0,0,0,0,0,,,,0,,,,,WX*4D
+///     <1> = Internal communication between microprocessors 
+///     <2> = Format code 
+///     <3> = Factory EEPROM 
+///     <4> = User EEPROM 
+///     <5> = Air temperature sensor 
+///     <6> = Plate temperature sensor x
+///     <7> = Standard relative humidity sensor 
+///     <8> = Barometric pressure sensor 
+///     <9> = Wind sensor 
+///     <10> = Compass sensor 
+///     <11> = GPS receiver 
+///     <12> = Attitude sensor 
+///     <13> = Rate gyro x
+///     <14> = Rain sensor  x
+///     <15> = Internal temperature Sensor  x
+///     <16> = Battery voltage Sensor  
+///     <17> = TBD Sensor  
+///     <18> = TBD Sensor  
+///     <19> = TBD Sensor  
+///     <20> = TBD Sensor  
+///     <21> = “WX” string that indicates POST results are for the family of  
+///     products described in this specification.
+/// 
+/// 
 // pub(crate) fn interpret_post(...) -> 
 //     Result<AirmarEvent, Box<dyn std::error::Error + Send + 'static>> {
 // pub(crate) fn interpret_post(nmea_sentence: String)
     // -> Result<AirmarEvent, Box<dyn std::error::Error>> {
 pub(crate) fn interpret_post(nmea_sentence: &str) -> anyhow::Result<AirmarEvent> {
-    println!("POST: {}", nmea_sentence);
+
     // split comma delimited String and check for 0 at select indices
     let fields: Vec<&str> = nmea_sentence.split(',').collect();
     let zero_indices: [usize; 13] = [2, 3, 4, 5, 6, 7, 8, 9, 10, 
         11, 12, 13, 16];
+        /// 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 17
 
     for &i in &zero_indices {
         if fields.get(i).is_none() {
+            logger::error("Malformed POST sentence");
             anyhow::bail!("Malformed POST sentence, missing field at index {}", i);
-            // return Err(format!("Malformed POST sentence, missing field at index {}", i).into());
         }
     }
     let all_zero: bool = zero_indices.iter().all(|&i| 
