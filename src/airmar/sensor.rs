a692e30a0d70327ea16b54d7c24458f90268cc40
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-07-07 09:14:36 -0700

    debug: output raw bytes for wind speed/direction

--- src/airmar/sensor.rs
+++ src/airmar/sensor.rs
@@ -159,6 +159,7 @@ impl AirmarSensorReal {
             if n == 0 {
                 continue;
             }
+            println!("{:?}", &buf[..n]);
 
             if let Err(e) = Self::process_expected_sentence(
                 &buf[..n], 
