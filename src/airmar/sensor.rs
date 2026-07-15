1698627ad11c630ab17ba14e494ea6955c96c697
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-07-15 06:50:13 -0700

    swapped altitude query for notification wait

--- src/airmar/sensor.rs
+++ src/airmar/sensor.rs
@@ -17,8 +17,7 @@ use tokio::io::{AsyncWriteExt, AsyncReadExt};
 use tokio::time::{timeout, Duration};
 
 use super::models::{AirmarEventTx, ExpectedSentence};
-use super::interpreter::{interpret_post, interpret_altitude, interpret_wimda,
-    interpret_gga};
+use super::interpreter::{interpret_post, interpret_altitude, interpret_wimda};
 use super::trait_airmar::AirmarT;
 use super::nmea_sentence::NMEASentenceRetriever;
 use crate::logger;
@@ -42,18 +41,16 @@ impl AirmarT for AirmarSensorReal {
             // determine proper nmea sentences from bytes
             let mut sentence_retriever = NMEASentenceRetriever::new();
 
-            // turn off all not needed sentences
-            // self.configure_sentence_transmissions(&mut port).await?;
-            // check gps values
-            self.verify_gga(&mut port, tx.clone(), &mut sentence_retriever).await?;
             // confirm airmar powered on correctly
-            // self.power_on_self_test(&mut port, tx.clone(), &mut sentence_retriever).await?;
-            // query for the altitude
-            // sentence_retriever.reset();
-            // self.detect_altitude(&mut port, tx.clone(), &mut sentence_retriever).await?;
+            self.power_on_self_test(&mut port, tx.clone(), &mut sentence_retriever).await?;
+            // get altitude notification
+            sentence_retriever.reset();
+            self.detect_altitude(&mut port, tx.clone(), &mut sentence_retriever).await?;
+            // turn off all not needed sentences
+            self.configure_sentence_transmissions(&mut port).await?;
             // listen for weather
-            // sentence_retriever.reset();
-            // self.detect_weather(&mut port, tx, &mut sentence_retriever).await?;
+            sentence_retriever.reset();
+            self.detect_weather(&mut port, tx, &mut sentence_retriever).await?;
 
             Ok(())
         })
@@ -66,48 +63,6 @@ impl AirmarSensorReal {
         -> anyhow::Result<()> {
         let bytes = Self::package_sentence("PAMTC,EN,ALL,0");
         port.write_all(&bytes).await?;
-        Ok(())
-    }
-
-    async fn verify_gga(&self, port: &mut SerialStream,
-        tx: AirmarEventTx, retriever: &mut NMEASentenceRetriever) -> anyhow::Result<()> {
-        let bytes = Self::package_sentence("PAMTC,EN,GGA,1,5");
-        println!("VERIFY GGA");
-        println!("{:?}", &bytes);
-        port.write_all(&bytes).await?;
-        // $PAMTC,EN,GSA,1
-        // $PAMTC,EN,RMC,1
-        let mut buf = [0u8; 64];
-        timeout(Duration::from_secs(20), async {
-            loop {
-                let n = port.read(&mut buf).await?;
-                if n == 0 {
-                    continue; // no bytes read
-                }
-                println!("{}", n);
-
-                match Self::process_expected_sentence(
-                    &buf[..n], 
-                    retriever, 
-                    ExpectedSentence::Gga, 
-                    interpret_gga, 
-                    &tx
-                ).await {
-                    Ok(true) => {
-                        return Ok::<(), anyhow::Error>(());
-                    }
-                    Ok(false) => {
-                        println!("sentence was not expected")
-                    }
-                    Err(e) => {
-                        println!("process_expected_sentence error: {:#}", e);
-                        return Err(e);
-                    }
-                }
-            }
-        }).await
-        .map_err(|_| anyhow::anyhow!("GGA query time out"))??;
-
         Ok(())
     }
 
@@ -151,14 +106,14 @@ impl AirmarSensorReal {
         Ok(())
     }
 
-    async fn detect_altitude_dprc(&self, port: &mut SerialStream, tx: AirmarEventTx, 
+    async fn detect_altitude(&self, port: &mut SerialStream, tx: AirmarEventTx, 
         retriever: &mut NMEASentenceRetriever) -> anyhow::Result<()> {
 
-        // send query for altitude response
-        let bytes = Self::package_sentence("PAMTC,ALT,Q");
+        // turn on notifications for altitude packet
+        let bytes = Self::package_sentence("PAMTC,EN,GGA,1,5");
         port.write_all(&bytes).await?;
 
-        // read query response for 5 seconds
+        // read notifications
         let mut buf = [0u8; 64];
         match timeout(Duration::from_secs(10), async {
             loop {
@@ -170,7 +125,7 @@ impl AirmarSensorReal {
                 if Self::process_expected_sentence(
                     &buf[..n], 
                     retriever, 
-                    ExpectedSentence::Alt, 
+                    ExpectedSentence::Gga, 
                     interpret_altitude, 
                     &tx
                 ).await? {
@@ -187,67 +142,8 @@ impl AirmarSensorReal {
                 anyhow::bail!("Altitude query timed out");
             }
         }
-        // .map_err(|_| anyhow::anyhow!("Altitude query time out"))??;
 
         Ok(())
-    }
-
-    async fn detect_altitude(&self, port: &mut SerialStream, tx: AirmarEventTx,
-        retriever: &mut NMEASentenceRetriever) -> anyhow::Result<()> {
-
-        let result = timeout(Duration::from_secs(60), async {
-            let mut buf = [0u8; 64];
-
-            loop {
-                // Query altitude
-                let bytes = Self::package_sentence("PAMTC,ALT,Q");
-                port.write_all(&bytes).await?;
-
-                // Wait for the response to this query
-                loop {
-                    let n = port.read(&mut buf).await?;
-
-                    if n == 0 {
-                        continue;
-                    }
-
-                    if Self::process_expected_sentence(
-                        &buf[..n],
-                        retriever,
-                        ExpectedSentence::Alt,
-                        interpret_altitude,
-                        &tx,
-                    )
-                    .await?
-                    {
-                        return Ok::<(), anyhow::Error>(());
-                    }
-
-                    // process_expected_sentence returned false:
-                    // - not the sentence we wanted
-                    // - OR altitude interpreter returned None
-                    //
-                    // Send another query after a delay.
-                    break;
-                }
-
-                tokio::time::sleep(Duration::from_millis(500)).await;
-            }
-        })
-        .await;
-
-        match result {
-            Ok(Ok(())) => Ok(()),
-
-            Ok(Err(e)) => {
-                eprintln!("Altitude detection failed: {:#}", e);
-                Err(e)
-            }
-
-            Err(_) => {
-                anyhow::bail!("Altitude query timed out waiting for valid altitude");
-            }
-        }
     }
 
     async fn detect_weather(&self, port: &mut SerialStream, tx: AirmarEventTx, 
